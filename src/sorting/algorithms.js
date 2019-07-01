a04c8832cf0c6630249bd82729f6ae3dbbdf1451
Giovanni Aguirre <giovanni.fi05@gmail.com>
2019-06-30 22:01:10 -0500

    :tada: Bubble sorted implemented

--- src/sorting/algorithms.js
+++ src/sorting/algorithms.js
@@ -1,4 +1,31 @@
 
+export const bubleSort = (numbers) => {
+    if (!numbers.length || numbers.length === 1) {
+        return numbers;
+    }
+
+    let completellySorted = false;
+    while (!completellySorted) {
+        completellySorted = true;
+
+        for (let i = 0; i < numbers.length; i++) {
+            if (numbers.length === i + 1) {
+                break;
+            }
+
+            if (numbers[i] > numbers[i + 1]) {
+                completellySorted = false;
+                
+                const aux = numbers[i];
+                numbers[i] = numbers[i + 1];
+                numbers[i + 1] = aux
+            }
+        }
+    }
+
+    return numbers;
+}
+
 export const selectionSort = (numbers) => {
     if (!numbers.length || numbers.length === 1) {
         return numbers;
--- src/sorting/algorithms.spec.js
+++ src/sorting/algorithms.spec.js
@@ -1,4 +1,4 @@
-import { selectionSort } from "./algorithms";
+import { bubleSort, selectionSort } from "./algorithms";
 
 describe('Selection sort', () => {
     test('Sort 1', () => {
@@ -34,5 +34,42 @@ describe('Selection sort', () => {
         let sorted   = [0, 12, 12, 12, 13, 14, 15];
 
         expect(selectionSort(unsorted)).toEqual(sorted);
+    });
+});
+
+describe('Buble sort', () => {
+    test('Sort 1', () => {
+        let unsorted = [5, 3, 2, 6, 98, 0];
+        let sorted   = [0, 2, 3, 5, 6, 98];
+        
+        expect(bubleSort(unsorted)).toEqual(sorted);
+    });
+
+    test('Sort 2', () => {
+        let unsorted = [9, 1, 8, 2, 3, 7, 6, 4, 5];
+        let sorted   = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+        
+        expect(bubleSort(unsorted)).toEqual(sorted);
+    });
+
+    test('Sort 3', () => {
+        let unsorted = [0, 0, 56, 6, 57, 6];
+        let sorted   = [0, 0, 6, 6, 56, 57];
+
+        expect(bubleSort(unsorted)).toEqual(sorted);
+    });
+
+    test('Sort 4', () => {
+        let unsorted = [15, 6, 7, 1];
+        let sorted   = [1, 6, 7, 15];
+
+        expect(bubleSort(unsorted)).toEqual(sorted);
+    });
+
+    test('Sort 5', () => {
+        let unsorted = [15, 14, 13, 12, 12, 12, 0];
+        let sorted   = [0, 12, 12, 12, 13, 14, 15];
+
+        expect(bubleSort(unsorted)).toEqual(sorted);
     });
 });
