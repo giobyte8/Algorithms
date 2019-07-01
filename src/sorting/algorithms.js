19ad59faed2cc49ea52edec9f0157d8a3312a3e9
Giovanni Aguirre <giovanni.fi05@gmail.com>
2019-07-01 01:32:46 -0500

    :tada: Merge sort implemented

--- src/sorting/algorithms.js
+++ src/sorting/algorithms.js
@@ -69,3 +69,45 @@ export const insertionSort = (numbers) => {
     
     return numbers;
 }
+
+export const mergeSort = (numbers) => {
+    if (!numbers.length || numbers.length === 1) {
+        return numbers;
+    }
+
+    const merge = (list1, list2) => {
+        let result = [];
+        let list1Idx = 0;
+        let list2Idx = 0;
+
+        while (list1Idx < list1.length && list2Idx < list2.length) {
+            if (list1[list1Idx] < list2[list2Idx]) {
+                result.push(list1[list1Idx]);
+                list1Idx++;
+            } else {
+                result.push(list2[list2Idx]);
+                list2Idx++;
+            }
+        }
+        
+        // Push possible pending elements from either list
+        result.push(...list1.slice(list1Idx));
+        result.push(...list2.slice(list2Idx));
+
+        return result;
+    }
+
+    const divideSort = (list) => {
+        if (list.length === 1) {
+            return list;
+        }
+
+        let middle = Math.ceil(list.length / 2);
+        let list1 = divideSort(list.slice(0, middle));
+        let list2 = divideSort(list.slice(middle));
+
+        return merge(list1, list2);
+    }
+
+    return divideSort(numbers);
+}
--- src/sorting/algorithms.spec.js
+++ src/sorting/algorithms.spec.js
@@ -1,4 +1,9 @@
-import { bubleSort, selectionSort, insertionSort } from "./algorithms";
+import {
+    bubleSort,
+    selectionSort,
+    insertionSort,
+    mergeSort
+} from "./algorithms";
 
 describe('Buble sort', () => {
     test('Sort 1', () => {
@@ -35,6 +40,19 @@ describe('Buble sort', () => {
 
         expect(bubleSort(unsorted)).toEqual(sorted);
     });
+
+    test('Sort 6 (Random numbers)', () => {
+        let length = Math.floor(Math.random() * 100 + 50)
+        let unsorted = [];
+        for (let i = 0; i < length; i++) {
+            unsorted.push(Math.floor(Math.random() * 100 + 50));
+        }
+
+        let sorted = bubleSort(unsorted);
+        for (let i = 1; i < length; i++) {
+            expect(sorted[i]).toBeGreaterThanOrEqual(sorted[i - 1]);
+        }
+    });
 });
 
 describe('Selection sort', () => {
@@ -72,9 +90,22 @@ describe('Selection sort', () => {
 
         expect(selectionSort(unsorted)).toEqual(sorted);
     });
+
+    test('Sort 6 (Random numbers)', () => {
+        let length = Math.floor(Math.random() * 100 + 50)
+        let unsorted = [];
+        for (let i = 0; i < length; i++) {
+            unsorted.push(Math.floor(Math.random() * 100 + 50));
+        }
+
+        let sorted = selectionSort(unsorted);
+        for (let i = 1; i < length; i++) {
+            expect(sorted[i]).toBeGreaterThanOrEqual(sorted[i - 1]);
+        }
+    });
 });
 
-describe('Selection sort', () => {
+describe('Insertion sort', () => {
     test('Sort 1', () => {
         let unsorted = [5, 3, 2, 6, 98, 0];
         let sorted   = [0, 2, 3, 5, 6, 98];
@@ -108,5 +139,68 @@ describe('Selection sort', () => {
         let sorted   = [0, 12, 12, 12, 13, 14, 15];
 
         expect(insertionSort(unsorted)).toEqual(sorted);
+    });
+
+    test('Sort 6 (Random numbers)', () => {
+        let length = Math.floor(Math.random() * 100 + 50)
+        let unsorted = [];
+        for (let i = 0; i < length; i++) {
+            unsorted.push(Math.floor(Math.random() * 100 + 50));
+        }
+
+        let sorted = insertionSort(unsorted);
+        for (let i = 1; i < length; i++) {
+            expect(sorted[i]).toBeGreaterThanOrEqual(sorted[i - 1]);
+        }
+    });
+});
+
+describe('Merge sort', () => {
+    test('Sort 1', () => {
+        let unsorted = [5, 3, 2, 6, 98, 0];
+        let sorted   = [0, 2, 3, 5, 6, 98];
+        
+        expect(mergeSort(unsorted)).toEqual(sorted);
+    });
+
+    test('Sort 2', () => {
+        let unsorted = [9, 1, 8, 2, 3, 7, 6, 4, 5];
+        let sorted   = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+        
+        expect(mergeSort(unsorted)).toEqual(sorted);
+    });
+
+    test('Sort 3', () => {
+        let unsorted = [0, 0, 56, 6, 57, 6];
+        let sorted   = [0, 0, 6, 6, 56, 57];
+
+        expect(mergeSort(unsorted)).toEqual(sorted);
+    });
+
+    test('Sort 4', () => {
+        let unsorted = [50, 26, 5, 26, 7];
+        let sorted   = [5, 7, 26, 26, 50];
+
+        expect(mergeSort(unsorted)).toEqual(sorted);
+    });
+
+    test('Sort 5', () => {
+        let unsorted = [15, 14, 13, 12, 12, 12, 0];
+        let sorted   = [0, 12, 12, 12, 13, 14, 15];
+
+        expect(mergeSort(unsorted)).toEqual(sorted);
+    });
+
+    test('Sort 6 (Random numbers)', () => {
+        let length = Math.floor(Math.random() * 100 + 50)
+        let unsorted = [];
+        for (let i = 0; i < length; i++) {
+            unsorted.push(Math.floor(Math.random() * 100 + 50));
+        }
+
+        let sorted = mergeSort(unsorted);
+        for (let i = 1; i < length; i++) {
+            expect(sorted[i]).toBeGreaterThanOrEqual(sorted[i - 1]);
+        }
     });
 });
