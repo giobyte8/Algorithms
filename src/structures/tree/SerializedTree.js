5f4f9f01180d046beb2cf7f9d211591f145366fc
Giovanni Aguirre <giovanni.fi05@gmail.com>
2019-07-09 20:22:54 -0500

    :rocket: Allow to serialize/deserialize a tree to/from string

--- src/structures/tree/SerializedTree.js
+++ src/structures/tree/SerializedTree.js
@@ -18,25 +18,65 @@ export default class SerializedTree extends BinaryTree {
      * by default
      */
     serialize(node = this.root) {
+        let jRoot = this._toJson(node);
+        return JSON.stringify(jRoot);
+    }
+
+    /**
+     * Serializes the specified node to a recursive json like
+     * {
+     *   v: <value>,
+     *   l: {<left offspring>}
+     *   r: {<right offspring>}
+     * }
+     * 
+     * @param {Node} node The node to serialize, root is used
+     * by default
+     */
+    _toJson(node = this.root) {
         if (node === null) return null;
 
         return {
             v: node.value,
-            l: node.left === null
-                ? null
-                : this.serialize(node.left),
-            r: node.right === null
-                ? null
-                : this.serialize(node.right)
+            l: this._toJson(node.left),
+            r: this._toJson(node.right)
         };
     }
 
     /**
-     * Takes the a serialized inorder representation
-     * and assembles the original tree
+     * Takes the a serialized tree and deserialize it to
+     * the original tree
      * @param {string} serialized Serialized tree
      */
-    deserializeFromInorder(serialized) {
+    static deserialize(serialized) {
+        let jRoot = JSON.parse(JSON.parse(serialized));
+        let root = SerializedTree._assemble(jRoot);
 
+        return new SerializedTree(root);
+    }
+
+    /**
+     * Takes a node in JSON notation and convert it into
+     * a Node instance, also, its offspring will be converted
+     * into nodes through recursion
+     * 
+     * @param {Object} jNode JSON serialized node
+     * @param {Object} jNode.v Node value
+     * @param {Object} jNode.l Left child
+     * @param {Object} jNode.r Rightchild
+     * @returns {Node} The node instance with its offspring
+     */
+    static _assemble(jNode) {
+        let node = new Node(jNode["v"]);
+
+        if (jNode["l"] !== null) {
+            node.left = SerializedTree._assemble(jNode["l"]);
+        }
+        
+        if (jNode["r"] !== null) {
+            node.right = SerializedTree._assemble(jNode["r"]);
+        }
+
+        return node;
     }
 }
--- src/structures/tree/SerializedTree.spec.js
+++ src/structures/tree/SerializedTree.spec.js
@@ -1,7 +1,7 @@
 import { Node } from "./TreeUtils";
 import SerializedTree from "./SerializedTree";
 
-describe("Serialized binary tree", () => {
+describe("Serialize binary tree", () => {
     test("Serialize tree 1", () => {
 
         // Assemble a tree like:
@@ -27,14 +27,15 @@ describe("Serialized binary tree", () => {
 
         let tree = new SerializedTree(n7);
         let serialized = tree.serialize();
+        let jTree = JSON.parse(serialized);
         
-        expect(serialized.v).toEqual(7);
-        expect(serialized.l.v).toEqual(4);
-        expect(serialized.l.l.v).toEqual(2);
-        expect(serialized.l.r.v).toEqual(5);
-        expect(serialized.r.v).toEqual(9);
-        expect(serialized.r.l.v).toEqual(8);
-        expect(serialized.r.r.v).toEqual(10);
+        expect(jTree.v).toEqual(7);
+        expect(jTree.l.v).toEqual(4);
+        expect(jTree.l.l.v).toEqual(2);
+        expect(jTree.l.r.v).toEqual(5);
+        expect(jTree.r.v).toEqual(9);
+        expect(jTree.r.l.v).toEqual(8);
+        expect(jTree.r.r.v).toEqual(10);
     });
 
     test("Serialize tree 2", () => {
@@ -54,11 +55,68 @@ describe("Serialized binary tree", () => {
 
         let tree = new SerializedTree(n7);
         let serialized = tree.serialize();
+        let jTree = JSON.parse(serialized);
 
-        expect(serialized.v).toEqual(7);
-        expect(serialized.l).toEqual(null);
-        expect(serialized.r.v).toEqual(9);
-        expect(serialized.r.l).toEqual(null);
-        expect(serialized.r.r.v).toEqual(10);
+        expect(jTree.v).toEqual(7);
+        expect(jTree.l).toEqual(null);
+        expect(jTree.r.v).toEqual(9);
+        expect(jTree.r.l).toEqual(null);
+        expect(jTree.r.r.v).toEqual(10);
+    });
+});
+
+describe("Deserialize binary tree", () => {
+    test("Deserialize tree 1", () => {
+
+        // Assemble a tree like:
+        //               7
+        //             /   \
+        //            4      9
+        //           / \    / \
+        //          2   5  8  10
+
+        let n2 = new Node(2);
+        let n5 = new Node(5);
+        let n8 = new Node(8);
+        let n10= new Node(10);
+        let n4 = new Node(4);
+        let n9 = new Node(9);
+        let n7 = new Node(7);
+        n4.left = n2;
+        n4.right = n5;
+        n9.left = n8;
+        n9.right = n10;
+        n7.left = n4;
+        n7.right = n9;
+
+        let tree = new SerializedTree(n7);
+        let serialized = JSON.stringify(tree.serialize());
+        let inorder = [2, 4, 5, 7, 8, 9, 10];
+
+        let restoredTree = SerializedTree.deserialize(serialized);
+        expect(restoredTree.traverseInorder()).toEqual(inorder);
+    });
+
+    test("Deserialize tree 2", () => {
+
+        // Assemble a tree like:
+        //               7
+        //                 \
+        //                   9
+        //                    \
+        //                    10
+
+        let n10= new Node(10);
+        let n9 = new Node(9);
+        let n7 = new Node(7);
+        n9.right = n10;
+        n7.right = n9;
+
+        let tree = new SerializedTree(n7);
+        let serialized = JSON.stringify(tree.serialize());
+        let inorder = [7, 9, 10];
+
+        let restoredTree = SerializedTree.deserialize(serialized);
+        expect(restoredTree.traverseInorder()).toEqual(inorder);    
     });
 });
